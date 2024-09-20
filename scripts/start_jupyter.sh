2338cd3747c7120f07c4761ddd45336a8eae07be
Bernardo Aguzzoli Heberle <bag222@g.uky.edu>
2024-09-20 09:32:26 -0400

    overlap updates

--- scripts/start_jupyter.sh
+++ scripts/start_jupyter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-singularity exec ../singularity_containers/2024-05-23_systematic_review.sif jupyter notebook --no-browser --port 1234
+singularity exec ../singularity_containers/2024-05-23_systematic_review.sif jupyter notebook --no-browser --port 1235
